bb03eec95b6dc78efd9c98e5af9c7e8c4c139c0f
Corrie Butler <corriebutler12@gmail.com>
2021-10-21 20:26:23 -0600

    added more to the fetch function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,15 @@
-var getUserRepos = function() {
-    fetch('https://api.github.com/users/octocat/repos');
+var getUserRepos = function(user) {
+    var apiURL = 'https://api.github.com/users/' + user + '/repos';
+
+
+    fetch(apiURL).then(function(response){
+        response.json().then(function(data){
+        console.log(data);
+    });
+});
 };
+
+var response = fetch('https://api.github.com/users/octocat/repos');
+console.log(response);
 
 getUserRepos();
