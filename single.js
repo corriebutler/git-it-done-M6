d721c8ade173d2718996d8ecdc3dc135a4ed8e5e
Corrie Butler <corriebutler12@gmail.com>
2021-11-03 20:02:15 -0600

    added the repo from the index file to the repo issues page

--- single.js
+++ single.js
@@ -2,6 +2,21 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+function getRepoName() {
+  var queryString = document.location.search;
+  var repoName = queryString.split('=')[1];
+  
+  if (repoName) {
+    getRepoIssues(repoName);
+
+    // Add repo name to the page header
+    repoNameEl.textContent = repoName;
+  }
+  else {
+    document.location.replace('./index.html');
+  }
+};
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -81,4 +96,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
